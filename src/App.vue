1f1e295937a483056dd84385f32775c2821cab31
Laura McCord <lauramccord705@gmail.com>
2019-01-30 12:32:32 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <i class="material-icons md-48"> assignment </i> <MyAwards />
+  </div>
+</template>
+
+<script>
+import MyAwards from "./components/MyAwards";
+
+export default {
+  name: "App",
+  components: {
+    MyAwards
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.material-icons.md-48 {
+  font-size: 48px;
+}
+.material-icons.md-64 {
+  font-size: 64px;
+}
+</style>
---
+++ src/components/MyAwards.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="awards">
+    <h2>{{ msg }}</h2>
+    <ul v-if="awards && awards.length">
+      <li v-for="(award, index) in awards" :key="index">
+        <div>{{ award.id }} {{ award.title }}</div>
+      </li>
+    </ul>
+
+    <ul v-if="errors && errors.length">
+      <li v-for="(error, index) in errors" :key="index">{{ error.message }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyAwards",
+  data() {
+    return {
+      msg: "My Active Awards",
+      awards: [
+        {
+          id: "00-0000",
+          sponsor: "Baylor College of Medicine",
+          title: "Training in Precision Environmental Health Sciences",
+          startDate: "01-JAN-2019",
+          endDate: "01-FEB-2023",
+          Amount: "150,000"
+        },
+        {
+          id: "00-0001",
+          sponsor: "Defense Agency",
+          title: "Predicting antibiotic resistance: Moving from reaction ....",
+          startDate: "01-MAR-2016",
+          endDate: "01-FEB-2019",
+          Amount: "1,000,000"
+        },
+        {
+          id: "00-0002",
+          sponsor: "National Institutes of Health (NIH)",
+          title: "Defining Evolutionary Trajectories...",
+          startDate: "01-JAN-2014",
+          endDate: "01-MAR-2019",
+          Amount: "500,000"
+        },
+        {
+          id: "00-0003",
+          sponsor: "The TAMU System Health Science Ctr",
+          title: "IBT Cell....",
+          startDate: "01-JAN-2016",
+          endDate: "01-JAN-2020",
+          Amount: "100,000"
+        },
+        {
+          id: "00-0004",
+          sponsor: "University of Texas Health Science Center-Houston",
+          title: "Antimicrobial peptide...",
+          startDate: "01-DEC-2018",
+          endDate: "01-FEB-2023",
+          Amount: "150,000"
+        }
+      ],
+      errors: []
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.awards {
+  max-width: 600px;
+  margin: auto;
+}
+li {
+  border: none;
+  width: 600px;
+  text-align: left;
+}
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
